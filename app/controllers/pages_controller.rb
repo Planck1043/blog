3e44760806def80964422bb517071fc4ee9f8d4f
Planck1043 <zhao1036713209@gmail.com>
2018-01-18 15:16:55 +0800

    5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
   def index
-    @pages = Article.all.paginate(:page => params[:page], :per_page => 5 )
+    @pages = Article.where("article_state = '公开' " ).paginate(:page => params[:page], :per_page => 5 )
+  end
+
+  def new
+    @sorts = Sort.all
+  end
+
+  def show
+    @sort = Sort.find(params[:id])
+    @articles = @sort.articles
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116085011) do
+ActiveRecord::Schema.define(version: 20180117151651) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -26,6 +26,18 @@ ActiveRecord::Schema.define(version: 20180116085011) do
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "ckeditor_assets", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "sorts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
